c2f93b2d5bf021c39985612d9668b18a187c6fd8
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2024-04-13 23:56:08 -0300

    feat: recriando validationbase

---
+++ backend/src/validation/ValidationBase.ts
@@ -0,0 +1,19 @@
+export default {
+    isValidEmail(email: string): boolean {
+        if (email.trim() === '') {
+            return false;
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        return emailRegex.test(email);
+    },
+
+    isFirstNameValid(firstName: string): boolean {
+        return (!!firstName && firstName.length > 1 && firstName.length < 50);
+    },
+
+    isLastNameValid(lastName: string): boolean {
+        return (!!lastName && lastName.length > 1 && lastName.length < 50);
+    }   
+}

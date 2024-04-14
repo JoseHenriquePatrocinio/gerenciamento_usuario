9dbd5d8b0b5b7be9b090cda2407d25347a3293d2
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2024-04-13 23:56:38 -0300

    feat: removendo uservalidation

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -1,34 +1,39 @@
-import { IUser } from "../model/UserModel"
-import UserValidation from "../validation/UserValidation";
+import UserModel from "../model/UserModel";
 import { PrismaClient } from "@prisma/client";
 import Util from "../util/Util";
+import Result from "../errorhandler/ErrorHandler";
+import ValidationBase from "../validation/ValidationBase";
 
 const prisma = new PrismaClient();
 
 export default {
-    async createUser(user: IUser) {
+    createUser: async (user: UserModel): Promise<Result<UserModel>> => {
 
-        UserValidation.validateUser(user);
+        if (!ValidationBase.isFirstNameValid(user.primeiro_nome)) {
+            return Result.fail<UserModel>('First name is invalid')
+        }
 
-        addDataHora(user)
+        if (!ValidationBase.isLastNameValid(user.ultimo_nome)) {
+            return Result.fail<UserModel>('Last name is invalid')
+        }
+
+        if (!ValidationBase.isValidEmail(user.email)) {
+            return Result.fail<UserModel>('Email is invalid');
+        }
 
         const createdUser = await prisma.user.create({
             data: {
                 id: user.id,
-                nome: user.nome,
+                primeiro_nome: user.primeiro_nome,
+                ultimo_nome: user.ultimo_nome,
                 email: user.email,
                 senha: user.senha,
                 active: true,
                 nivel_acesso: user.nivel_acesso,
-                created_at: user.created_at,
+                created_at: Util.getCurrentDateTime(),
             },
         });
 
-        return createdUser;
+        return Result.ok(createdUser);
     },
-
 };
-
-function addDataHora(user: IUser): void {
-    user.created_at = Util.getCurrentDateTime();
-}

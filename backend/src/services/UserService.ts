b497007d1a57e2e3a325c07736e890bfdd85f9eb
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2024-04-14 08:32:33 -0300

    feat: add generic response e passwordhash

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -3,41 +3,60 @@ import { PrismaClient } from "@prisma/client";
 import Util from "../util/Util";
 import Result from "../errorhandler/ErrorHandler";
 import ValidationBase from "../validation/ValidationBase";
+import TResponse from "../model/ResponseModel";
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 export default {
-    createUser: async (user: UserModel): Promise<Result<UserModel>> => {
+    createUser: async (user: UserModel): Promise<Result<TResponse>> => {
+        try {
+            //#region Validate
 
-        //#region Validate
+            if (!ValidationBase.isFirstNameValid(user.primeiro_nome)) {
+                return Result.fail<TResponse>('First name is invalid')
+            }
 
-        if (!ValidationBase.isFirstNameValid(user.primeiro_nome)) {
-            return Result.fail<UserModel>('First name is invalid')
+            if (!ValidationBase.isLastNameValid(user.ultimo_nome)) {
+                return Result.fail<TResponse>('Last name is invalid')
+            }
+
+            if (!ValidationBase.isValidEmail(user.email)) {
+                return Result.fail<TResponse>('Email is invalid');
+            }
+
+            if (!user.senha) {
+                return Result.fail<TResponse>('Password is invalid');
+            }
+
+            //#endregion
+
+            const createdUser = await prisma.user.create({
+                data: {
+                    id: user.id,
+                    primeiro_nome: user.primeiro_nome,
+                    ultimo_nome: user.ultimo_nome,
+                    email: user.email,
+                    senha: await bcrypt.hash(user.senha, 10),
+                    active: true,
+                    nivel_acesso: user.nivel_acesso,
+                    created_at: Util.getCurrentDateTime(),
+                },
+            });
+
+            const response: TResponse = {
+                data: {
+                    id: user.id,
+                    primeiro_nome: user.primeiro_nome,
+                    ultimo_nome: user.ultimo_nome,
+                    email: user.email,
+                    nivel_acesso: user.nivel_acesso,
+                }
+            };
+
+            return Result.ok<TResponse>(response);
+        } catch (error) {
+            return Result.fail<TResponse>('An error occurred while creating the user');
         }
-
-        if (!ValidationBase.isLastNameValid(user.ultimo_nome)) {
-            return Result.fail<UserModel>('Last name is invalid')
-        }
-
-        if (!ValidationBase.isValidEmail(user.email)) {
-            return Result.fail<UserModel>('Email is invalid');
-        }
-
-        //#endregion
-
-        const createdUser = await prisma.user.create({
-            data: {
-                id: user.id,
-                primeiro_nome: user.primeiro_nome,
-                ultimo_nome: user.ultimo_nome,
-                email: user.email,
-                senha: user.senha,
-                active: true,
-                nivel_acesso: user.nivel_acesso,
-                created_at: Util.getCurrentDateTime(),
-            },
-        });
-
-        return Result.ok(createdUser);
     }
 };

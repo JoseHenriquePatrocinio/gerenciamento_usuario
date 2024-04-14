6962839cbfb1be8c0b3b6f716c4b1c992ebb0b42
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2024-04-13 21:29:07 -0300

    fix: correcao class model

--- backend/src/model/UserModel.ts
+++ backend/src/model/UserModel.ts
@@ -1,4 +1,4 @@
-export interface IUser {
+export default class UserModel {
     id: number
     nome: string
     email: string
@@ -6,4 +6,21 @@ export interface IUser {
     active: boolean
     nivel_acesso: number
     created_at: string
+
+    constructor(
+        id: number,
+        nome: string,
+        email: string,
+        senha: string,
+        active: boolean,
+        nivel_acesso: number,
+        created_at: string) {
+        this.id = id
+        this.nome = nome
+        this.email = email
+        this.senha = senha
+        this.active = active
+        this.nivel_acesso = nivel_acesso
+        this.created_at = created_at
+    }
 }

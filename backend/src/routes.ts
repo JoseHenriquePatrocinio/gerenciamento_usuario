08a7f80effc562ecf09c006eea419cc42d4fbe6d
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2024-04-14 22:46:54 -0300

    feat: add auth middleware

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import UserController from "./controllers/UserController";
 import AuthController from "./controllers/AuthController";
+import authMiddleware from "./middlewares/authMiddleware";
 
 const router = Router();
 
 router.post("/login", AuthController.login);
+
+router.use(authMiddleware);
 
 router.post("/createuser", UserController.createUser);
 router.put("/updateuserbyemail", UserController.updateUserByEmail);
